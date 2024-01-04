e6ce7dfa632676e508df69944384fe513bf36693
rugby8724 <jeremiah.d.wise@gmail.com>
2024-01-04 16:20:13 -0600

    add Pool

--- db.js
+++ db.js
@@ -1,9 +1,17 @@
 // set a configuration for what or where we want
 // to connect the database 
 
-const Pool = require("pg").Pool
+import pg from 'pg'
+const {Pool} = pg;
+
+const password = process.env.POSTGES_ACCESS
 
 const pool = new Pool({
   user: "postgres",
-  
+  password: password,
+  database: "todo_database",
+  host: "localhost",
+  port: 5432
 })
+
+export default pool;
--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-const express = require('express')
+import express from 'express'
+import dotenv from 'dotenv'
+
+import pool from './db'
+
+dotenv.config()
 
 const app = express()
+
+//ROUTES
+
+//get all todos
+
+// get a todo
+
+// create a todo
+
+// update a todo
+
+// delete a todo
+
+// allows us to parse raw json
+app.use(express.json())
+
+// allows us to send form data
+app.use(express.urlencoded({extended: true}))
 
 app.listen(3000, () =>{
   console.log("server is listening on port 3000")
