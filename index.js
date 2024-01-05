a2f9f874d44c23b7b6a0e3900b48e6acf5054a17
rugby8724 <jeremiah.d.wise@gmail.com>
2024-01-05 17:18:31 -0600

    end of postgres practice

--- index.js
+++ index.js
@@ -33,6 +33,7 @@ app.get("/todos/:id", async (req, res) => {
   const {id} = req.params
   try {
     const todo = await pool.query("SELECT * FROM todo WHERE todo_id = $1", [id])
+    res.json(todo.rows[0])
   } catch (err) {
     console.error(err.message)
     res.status(500).send("Internal Server Error")
@@ -53,8 +54,26 @@ app.post("/todos", async (req, res) => {
 })
 
 // update a todo
+app.put("/todos/:id", async (req, res)=> {
+  try {
+    const {id} = req.params //WHERE
+    const {description} = req.body //SET
+
+    const updateTodo = await pool.query("UPDATE todo SET description = $1 WHERE todo_id = $2", [description, id])
+
+    res.json("Todo was updated")
+  } catch (err) {
+    console.error(err.message)
+    res.status(500).send("Internal Server Error")
+  }
+})
 
 // delete a todo
+app.delete("/todos/:id", async (req, res) =>{
+  const {id} = req.params
+  const deleteTodo = await pool.query("DELETE FROM todo WHERE todo_id = $1", [id])
+  res.json('Todo was deleted')
+})
 
 app.listen(3000, () => {
   console.log("server is listening on port 3000")
